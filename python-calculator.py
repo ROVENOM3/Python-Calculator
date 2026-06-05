24756118c17a8e9d3ba9b3656ff597609e411931
ROVENOM3 <aryangoyal784@gmail.com>
2026-06-05 17:31:02 +0530

    Python Calculator

---
+++ python-calculator.py
@@ -0,0 +1,41 @@
+while(True):
+    print("""
+          1. Addition
+          2. Subtraction
+          3. Multiply
+          4. Division
+          5. Power
+          6. Modulus
+          7. Floor Division
+          8. Exit""")
+    ch = int(input("Enter your choice: "))
+    if(ch == 1):
+        a = int(input("Enter 1st Number: "))
+        b = int(input("Enter 2nd Number: "))
+        pass
+    elif(ch == 2):
+        a = int(input("Enter 1st Number: "))
+        b = int(input("Enter 2nd Number: "))
+        pass
+    elif(ch == 3):
+        a = int(input("Enter 1st Number: "))
+        b = int(input("Enter 2nd Number: "))
+        pass
+    elif(ch == 4):
+        a = int(input("Enter 1st Number: "))
+        b = int(input("Enter 2nd Number: "))
+        pass
+    elif(ch == 5):
+        a = int(input("Enter 1st Number: "))
+        b = int(input("Enter the power for 1st No:  "))
+        pass
+    elif(ch == 6):
+        a = int(input("Enter 1st Number: "))
+        b = int(input("Enter 2nd Number: "))
+        pass
+    elif(ch == 7):
+        a = int(input("Enter 1st Number: "))
+        b = int(input("Enter 2nd Number: "))
+        pass  
+    elif(ch == 8):
+        break      
