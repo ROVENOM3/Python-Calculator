d22c3c6088d0698bf56e69e98b48272f1c725511
ROVENOM3 <aryangoyal784@gmail.com>
2026-06-06 22:54:30 +0530

    Screenshot Folder

--- python-calculator.py
+++ python-calculator.py
@@ -36,6 +36,6 @@ while(True):
     elif(ch == 7):
         a = int(input("Enter 1st Number: "))
         b = int(input("Enter 2nd Number: "))
-        print(a//b)  
+        print("Floor Division Answer: ",a//b)  
     elif(ch == 8):
         break      
